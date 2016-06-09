2dc8e5fc9ffd05c30161d2422b05b8b504205567
Al Johri <al.johri@gmail.com>
2016-06-09 18:39:10 -0400

    fix typo, simplernlg not simplenlg

--- writer3/app.rb
+++ writer3/app.rb
@@ -1,16 +1,16 @@
 require 'simplernlg'
 
-puts SimpleNLG::NLG.render("mary is happy")
+puts SimplerNLG::NLG.render("mary is happy")
 
-puts SimpleNLG::NLG[subject:%w[Kurt Linda], verb:"invite", object:%w[Jake Roswitha], tense:"past", negation:true]
+puts SimplerNLG::NLG[subject:%w[Kurt Linda], verb:"invite", object:%w[Jake Roswitha], tense:"past", negation:true]
 
 puts SimplerNLG::NLG.render{
   phrase(
-    s:"Igor", 
-    v:[ 
+    s:"Igor",
+    v:[
       phrase(v:"gain",t:"past",perfect:true, o:"wisdom"),
       phrase(v:pre_mod("teach","now"),progressive:true)
-    ], 
+    ],
     o:"everyone")
 }
 
